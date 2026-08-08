ea81b6be87c4fa46e34a025bf47024db45835c3e
zhuangqingshu <79222166@qq.com>
2026-08-09 00:13:15 +0800

    Fix switch platform schema and imports

--- components/ble_mesh_gateway/switch/__init__.py
+++ components/ble_mesh_gateway/switch/__init__.py
@@ -7,25 +7,27 @@ from __future__ import annotations
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import switch
-from esphome.const import CONF_ID, CONF_SLOT
+from esphome.const import CONF_ID
 
 from .. import ble_mesh_gateway_ns
 
 DEPENDENCIES = ["ble_mesh_gateway"]
 
-BleMeshSwitch = ble_mesh_gateway_ns.class_(
-    "BleMeshSwitch", switch.Switch
-)
+CONF_SLOT = "slot"
+
+BleMeshSwitch = ble_mesh_gateway_ns.class_("BleMeshSwitch", switch.Switch)
 
 CONFIG_SCHEMA = switch.switch_schema(BleMeshSwitch).extend(
     {
+        cv.GenerateID(): cv.declare_id(BleMeshSwitch),
         cv.Required(CONF_SLOT): cv.uint8_t,
     }
 )
 
 
 async def to_code(config):
-    var = await switch.new_switch(config)
+    var = cg.new_Pvariable(config[CONF_ID])
+    await switch.register_switch(var, config)
     cg.add(var.set_slot(config[CONF_SLOT]))
     gateway = await cg.get_variable("ble_mesh_gw")
     cg.add(var.set_gateway(gateway))
